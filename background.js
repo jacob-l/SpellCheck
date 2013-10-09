3de611cdca29155b51746868b689287cb373ecf8
Jacob <you@example.com>
2013-10-09 23:08:09 +0700

    Context menu

--- background.js
+++ background.js
@@ -1,3 +1,29 @@
 /*
  * http://developer.chrome.com/extensions/background_pages.html - фоновая страница
  */
+
+function contextMenuHandler(info, tab) {
+    var text = info.selectionText;
+
+    console.log(text);
+};
+
+/*
+ * http://developer.chrome.com/extensions/runtime.html#event-onInstalled
+ *
+ * Это событие возникает, когда расширение впервые устанавливается,
+ * обновляется до новой версии или Google Chrome обновляется до новой версии.
+ */
+chrome.runtime.onInstalled.addListener(function() {
+    /*
+     * http://developer.chrome.com/extensions/contextMenus.html#method-create
+     *
+     * Добавляем элемент в контекстное меню.
+     */
+    chrome.contextMenus.create({
+        title: 'Проверить правописание',
+        //Элемент появится только тогда, когда есть выделенный текст на странице
+        contexts: ['selection'],
+        onclick: contextMenuHandler
+    })
+});
